30885c6d3e25e99eae6574a7d979021b562b9e77
xzc21 <itswilltian@gmail.com>
2022-09-25 13:29:30 -0400

    Added UI layout

--- middleware.ts
+++ middleware.ts
@@ -4,7 +4,7 @@ const signedinPages = ["/", "/playlist", "/library"];
 
 export default function middleware(req) {
   if (signedinPages.find((p) => p === req.nextUrl.pathname)) {
-    const token = req.cookies.get("TRAX_ACCESS_TOKEN");
+    const token = req.cookies.get("MUSIFY_ACCESS_TOKEN");
     if (!token) {
       return NextResponse.redirect(new URL("/signin", req.url));
     }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,18 @@
+import GradientLayout from "../components/gradientLayout";
+
 const Home = () => {
-  return <div>home</div>;
+  return (
+    <GradientLayout
+      roundImage
+      color="green"
+      subtitle="profile"
+      title="William"
+      description="15 public playlists"
+      image="https://ikzttp.mypinata.cloud/ipfs/QmYDvPAXtiJg7s8JdRBSLWdgSphQdac8j1YuQNNxcGE1hg/571.png"
+    >
+      <div>Home page</div>
+    </GradientLayout>
+  );
 };
 
 export default Home;
