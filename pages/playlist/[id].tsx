2d9c7de90bf010341535d96e35dae32dd74a5ebd
xzc21 <itswilltian@gmail.com>
2022-09-26 15:49:49 -0400

    Added state and player bar UI

--- pages/playlist/[id].tsx
+++ pages/playlist/[id].tsx
@@ -34,11 +34,19 @@ const Playlist = ({ playlist }) => {
 };
 
 export const getServerSideProps = async ({ query, req }) => {
-  const { id } = validateToken(req.cookies.MUSIFY_ACCESS_TOKEN);
+  let user;
+  try {
+    user = validateToken(req.cookies.MUSIFY_ACCESS_TOKEN);
+  } catch (e) {
+    return {
+      permanent: false,
+      path: "/signin",
+    };
+  }
   const [playlist] = await prisma.playlist.findMany({
     where: {
       id: +query.id,
-      userId: id,
+      userId: user.id,
     },
     include: {
       songs: {
