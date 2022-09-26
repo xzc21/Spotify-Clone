b086776a5fb02ddda7339d394cd4c887e1f8605c
xzc21 <itswilltian@gmail.com>
2022-09-25 23:23:28 -0400

    Added artist list UI

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,55 @@
+import { Box, Flex, Text } from "@chakra-ui/layout";
+import { Image } from "@chakra-ui/react";
 import GradientLayout from "../components/gradientLayout";
+import prisma from "../lib/prisma";
 
-const Home = () => {
+const Home = ({ artists }) => {
   return (
     <GradientLayout
       roundImage
-      color="green"
+      color="blue"
       subtitle="profile"
       title="William"
       description="15 public playlists"
       image="https://ikzttp.mypinata.cloud/ipfs/QmYDvPAXtiJg7s8JdRBSLWdgSphQdac8j1YuQNNxcGE1hg/571.png"
     >
-      <div>Home page</div>
+      <Box color="white" paddingX="40px">
+        <Box marginBottom="40px">
+          <Text fontSize="2xl" fontWeight="bold">
+            Top artist this month
+          </Text>
+          <Text fontSize="md"> Only visible to you</Text>
+        </Box>
+        <Flex>
+          {artists.map((artist) => (
+            <Box paddingX="10px" width="20%">
+              <Box bg="gray.800" borderRadius="4px" padding="15px" width="100%">
+                <Image
+                  src={
+                    "https://ui-avatars.com/api/?background=0D8ABC&size=250&name=" +
+                    artist.name
+                  }
+                  borderRadius="100%"
+                />
+                <Box marginTop="20px">
+                  <Text fontSize="large">{artist.name}</Text>
+                  <Text fontSize="small">Artist</Text>
+                </Box>
+              </Box>
+            </Box>
+          ))}
+        </Flex>
+      </Box>
     </GradientLayout>
   );
+};
+
+export const getServerSideProps = async () => {
+  const artists = await prisma.artist.findMany({});
+
+  return {
+    props: { artists },
+  };
 };
 
 export default Home;
